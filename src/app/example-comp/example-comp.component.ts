3567557314fac7575d872a984b99116bc966444e
IamArshadAli <arshadalikaldane@gmail.com>
2024-01-23 21:48:48 +0530

    new component created

---
+++ src/app/example-comp/example-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example-comp',
+  standalone: true,
+  imports: [],
+  templateUrl: './example-comp.component.html',
+  styleUrl: './example-comp.component.css'
+})
+export class ExampleCompComponent {
+
+}
